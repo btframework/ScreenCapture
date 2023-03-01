2608300d3dcf1e79e01e73b617255b63cd289c38
btframework <44606899+btframework@users.noreply.github.com>
2023-03-01 14:39:20 +0300

    License

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2023 Marina Petrichenko
+ * -------------------------------------
+ *   marina@btframework.com
+ *   https://www.facebook.com/marina.petrichenko.1
+ *   
+ *   It is free for non-commercial and/or education use only.
+ *   
+ *   If you use all or any part of this code in your commercial, non-commercial, education, open source
+ *   or any other application your application/project/product/work source code must also be opened.
+ *   
+ *   If you want to use it in any kind of closed source product contact me and we discuss the price.
+ *   
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
